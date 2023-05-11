f0626613c981215e3dc5524adc3ec29d20dceede
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2023-05-11 16:32:55 +0200

    feat: create elements

--- minesweeper/src/index.js
+++ minesweeper/src/index.js
@@ -1,4 +1,18 @@
 import { answer } from './module.js'
 import './style.css'
 
+const sweeperTitle = document.createElement("h1");
+const sweeperSubtext = document.createElement("p");
+const sweeperBoard = document.createElement("div");
 
+
+sweeperTitle.className = "sweeper_title";
+sweeperSubtext.className = "sweeper_subtext";
+sweeperBoard.className = "sweeper_board";
+
+sweeperTitle.innerHTML = "RSS Minesweeper"
+sweeperSubtext.innerHTML = "Mines Left: 10"
+
+
+
+document.body.append(sweeperTitle, sweeperSubtext, sweeperBoard);
