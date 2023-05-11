b23166974a2f9a349c9ed7dc6d6ac37eab51fc01
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2023-05-11 19:08:35 +0200

    feat: create element board

---
+++ minesweeper/src/board.js
@@ -0,0 +1,33 @@
+export const statusTile = {
+    HIDDEN: "hidden",
+    MINE: "mine",
+    NUMBER: "number",
+    MARKED: "marked",
+}
+
+export default function createBoard(boardSize, numberOfMines) {
+    const board = [];
+
+    for (let x = 0; x < boardSize; x++) {
+        const row = [];
+        for (let y = 0; y < boardSize; y++) {
+            const elem = document.createElement("div");
+            elem.dataset.status = statusTile.HIDDEN;
+            const tile = {
+                elem,
+                x,
+                y,
+
+                get status() {
+                    return this.elem.dataset.status;
+                },
+                set status(value) {
+                    this.elem.dataset.status = value;
+                }
+            };
+            row.push(tile);
+        }
+        board.push(row);
+    }
+    return board;
+}
--- minesweeper/src/index.js
+++ minesweeper/src/index.js
@@ -1,18 +1,28 @@
-import { answer } from './module.js'
-import './style.css'
+import createBoard, { statusTile } from './board.js';
+import './style.css';
 
 const sweeperTitle = document.createElement("h1");
 const sweeperSubtext = document.createElement("p");
 const sweeperBoard = document.createElement("div");
-
+const boardS = 10;
+const boardMines = 10;
 
 sweeperTitle.className = "sweeper_title";
 sweeperSubtext.className = "sweeper_subtext";
+
 sweeperBoard.className = "sweeper_board";
 
-sweeperTitle.innerHTML = "RSS Minesweeper"
-sweeperSubtext.innerHTML = "Mines Left: 10"
-
-
+sweeperTitle.innerHTML = "RSS Minesweeper";
+sweeperSubtext.innerHTML = "Mines Left: 10";
 
 document.body.append(sweeperTitle, sweeperSubtext, sweeperBoard);
+
+const board = createBoard(boardS, boardMines);
+console.log(board)
+board.forEach(row => {
+    row.forEach(tile => {
+        sweeperBoard.append(tile.elem)
+    })
+})
+
+sweeperBoard.style.setProperty("--size", boardS);
